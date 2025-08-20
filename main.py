85a1343febe1cf29b58eb8029e8dd6c1ef247a90
GhosTHaise <ghostrex2@gmail.com>
2025-08-20 20:42:35 +0300

    feat : chat ollama

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from langchain-pydantic!")
+import os
+from langchain.llms import Ollama
+from langchain.chat_models import ChatOllama
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate
+    )
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
 
+model = ChatOllama(model="llama3")
 
-if __name__ == "__main__":
-    main()
