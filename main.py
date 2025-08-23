0c3a8d95daaa336d3c7ff65f8426cba100c530b3
GhosTHaise <ghostrex2@gmail.com>
2025-08-23 21:33:38 +0300

    PydanticOutputParser

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ class Players(BaseModel):
 parser = PydanticOutputParser(pydantic_object=Players)
 
 print(parser.get_format_instructions())
+
+# --- DEFINE PROMPT ---
+human_prompt = HumanMessagePromptTemplate.from_template("{request}\n{format_instructions}") 
+chat_prompt = ChatPromptTemplate.from_messages([human_prompt])
+
+request = chat_prompt.format_prompt(
+    request="Give me the top 100 players in the world",
+    format_instructions=parser.get_format_instructions()
+).to_messages()
+
+# --- CALL MODEL ---
+response = model(request, temperature=0)
+
+# --- PARSE RESPONSE ---
+parsed_response = parser.parse(response.content) # Player class object
+print(parsed_response)
