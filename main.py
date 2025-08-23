260e7eab96115454572ec506a245f0225ca6908b
GhosTHaise <ghostrex2@gmail.com>
2025-08-23 21:20:31 +0300

    PydanticOutputParser

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-from langchain.llms import Ollama
-from langchain.chat_models import ChatOllama
+from langchain_community.llms import Ollama
+from langchain_ollama import ChatOllama
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate
@@ -10,3 +10,12 @@ from pydantic import BaseModel, Field
 
 model = ChatOllama(model="llama3")
 
+
+# --- DEFINE OUTPUT DATA ---
+class Players(BaseModel):
+    values : list = Field(description="List of players with name and nationality")
+    city : str = Field(description="Give me the most popular country across the results")
+    
+parser = PydanticOutputParser(pydantic_object=Players)
+
+print(parser.get_format_instructions())
