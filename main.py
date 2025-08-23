2632d782cdabb91b2a61e21f47a27e8fb9bdc1d7
GhosTHaise <ghostrex2@gmail.com>
2025-08-23 21:49:57 +0300

    feat : create a data frame

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from langchain.prompts.chat import (
 from langchain.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 
-model = ChatOllama(model="llama3")
+model = ChatOllama(model="llama3",temperature=0)
 
 
 # --- DEFINE OUTPUT DATA ---
 class Players(BaseModel):
+    # The `values` field in the `Players` class is a list that contains information about players,
+    # including their name and nationality. It is used to store the list of players retrieved from the
+    # model's response.
     values : list = Field(description="List of players with name and nationality")
     city : str = Field(description="Give me the most popular country across the results")
     
@@ -25,13 +28,23 @@ human_prompt = HumanMessagePromptTemplate.from_template("{request}\n{format_inst
 chat_prompt = ChatPromptTemplate.from_messages([human_prompt])
 
 request = chat_prompt.format_prompt(
-    request="Give me the top 100 players in the world",
+    request="Give me the top 10 players in the world",
     format_instructions=parser.get_format_instructions()
 ).to_messages()
 
 # --- CALL MODEL ---
-response = model(request, temperature=0)
+response = model.invoke(request)
 
 # --- PARSE RESPONSE ---
 parsed_response = parser.parse(response.content) # Player class object
-print(parsed_response)
+print(f'results_values = {parsed_response}, {type(parsed_response)}')
+
+
+import pandas as pd
+
+results_dataframe = pd.DataFrame.from_dict(parsed_response.values)
+
+print(results_dataframe.head(10));
+print(results_dataframe.shape);
+
+print(f'The most popular city across the results is {parsed_response.city}')
